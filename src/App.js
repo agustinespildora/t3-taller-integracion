1c9b77c5d4b727bf0df0151b69e3b6e813abd741
agustinespildora <jaespildora@uc.cl>
2021-05-20 21:16:00 -0400

    test deploy

--- src/App.js
+++ src/App.js
@@ -5,22 +5,21 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <nav>
+          <ul id="navigation">
+              <li>
+                  <Link to="/flights">Flights</Link>
+              </li>
+              {/* <li>
+                  <Link to="/about">About</Link>
+              </li>
+              <li>
+                  <Link to="/contact">Contact</Link>
+              </li> */}
+          </ul>
+      </nav>
+  </div>
   );
 }
 
